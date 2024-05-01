eaea092f9290283de53063e4b70df434fcc8ed25
spark-06 <>
2024-05-01 16:32:04 +0900

    20240501 P96(４章までクリア)

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -10,27 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
-    
-    let quizzes = ["猫は人間を超でっかいネコだと思っている？",//0番
-                   "イヌは食べ物の美味しさを味よりも匂いで判断している？",//1番
-                   "トラのしましま模様は皮膚まで繋がっていない？"]//2番
-    
+
+    //下記の右辺はクラスと呼ばれる
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        var str = ""//空文字
-        str = self.quizzes[0]
-        self.label.text = str
-        
+
     }
     
     @IBAction func pressButton(_ sender: Any) {
-        
-        //Buttonクリックでテキストの変更
-        //Buttonのクリックで文字色の変更
-        self.label.text = "ボタンを押しました"
-        self.label.textColor = UIColor.red
+
     }
     
 }
