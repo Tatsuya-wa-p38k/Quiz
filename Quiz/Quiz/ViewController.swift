7208bb854f017041e4ecb1444c6b84dee3e985ff
spark-06 <>
2024-05-02 16:43:39 +0900

    教科書P142まで終了

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -11,19 +11,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     
-    let quizzes = ["猫は人間を超でっかいネコだと思っている？",//0番
-                   "イヌは食べ物の美味しさを味よりも匂いで判断している？",//1番
-                   "トラのしましま模様は皮膚まで繋がっていない？"]//2番
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        var str = ""//空文字
-        str = self.quizzes[0]
-        self.label.text = str
-        
-    }
+//    let quizzes = ["猫は人間を超でっかいネコだと思っている？",//0番
+//                   "イヌは食べ物の美味しさを味よりも匂いで判断している？",//1番
+//                   "トラのしましま模様は皮膚まで繋がっていない？"]//2番
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//
+//        var str = ""//空文字
+//        str = self.quizzes[0]
+//        self.label.text = str
+//
+//    }
     
     @IBAction func pressButton(_ sender: Any) {
         
