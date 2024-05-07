334a1892a3d4a13e609b8ff418110ee3b7536c54
spark-06 <>
2024-05-07 16:01:21 +0900

    P172 第６章まで終了 ⭐️エラーでシュミレーターが落ちる

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -1,37 +1,35 @@
+
 //
 //  ViewController.swift
-//  Quiz
+//  aaa
 //
-//  Created by spark-06 on 2024/04/25.
+//  Created by spark-06 on 2024/05/07.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var label: UILabel!
-    
-//    let quizzes = ["猫は人間を超でっかいネコだと思っている？",//0番
-//                   "イヌは食べ物の美味しさを味よりも匂いで判断している？",//1番
-//                   "トラのしましま模様は皮膚まで繋がっていない？"]//2番
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//
-//        var str = ""//空文字
-//        str = self.quizzes[0]
-//        self.label.text = str
-//
-//    }
-    
-    @IBAction func pressButton(_ sender: Any) {
-        
-        //Buttonクリックでテキストの変更
-        //Buttonのクリックで文字色の変更
-        self.label.text = "ボタンを押しました"
-        self.label.textColor = UIColor.red
+    @IBOutlet weak var nameTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
-    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let quizViewController = segue.destination as? QuizViewController {
+            if let text = self.nameTextField.text {
+                quizViewController.nameText = text
+            }
+        }
+    }
+
+    @IBAction func pressButton(_ sender: Any) {
+
+    }
+
+
 }
 
